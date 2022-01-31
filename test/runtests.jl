cf7f527c8099f3c49cb004420fa1c6ed83bfbfc0
Valentin Churavy <v.churavy@gmail.com>
2022-01-30 22:51:37 -0500

    Add CI for Enzyme

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,23 @@
+
+# HACK: work around Pkg.jl#2500
+test_project = Base.active_project()
+preferences_file = joinpath(dirname(@__DIR__), "LocalPreferences.toml")
+test_preferences_file = joinpath(dirname(test_project), "LocalPreferences.toml")
+if isfile(preferences_file) && !isfile(test_preferences_file)
+    cp(preferences_file, test_preferences_file)
+end
+
 using LULESH
 using Test
 using MPI
+
+if parse(Bool, get(ENV, "ENZYME_CI", "false"))
+    using Enzyme_jll
+    @info "Testing against" Enzyme_jll.libEnzyme
+    args = `--enzyme`
+else
+    args = ``
+end
 
 function run_example(name, args; nranks=nothing)
     example = joinpath(@__DIR__, "..", "examples", name)
@@ -18,8 +35,8 @@ end
 
 @testset "examples" begin
     @testset "benchmark.jl" begin
-        run_example("benchmark.jl", `-s 45`)
-        run_example("benchmark.jl", `-s 45`; nranks=1)
-        run_example("benchmark.jl", `-s 45`; nranks=8)
+        run_example("benchmark.jl", `-s 45 $(args)`)
+        run_example("benchmark.jl", `-s 45 $(args)`; nranks=1)
+        run_example("benchmark.jl", `-s 45 $(args)`; nranks=8)
     end
 end
