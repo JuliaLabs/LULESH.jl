59f0147f23f503a0ebf6742149485db04cb0dc09
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-01-30 23:58:23 -0500

    Add CI for Enzyme (#27)
    * Add CI for Enzyme
    
    * Check-in CI
    
    * don't require Enzyme to be already installed
    
    * make provide Enzyme on normal CI as well
    
    * copy LocalPreferences into the right place
    
    * fixup! Add CI for Enzyme

--- setup.jl
+++ setup.jl
@@ -2,13 +2,19 @@
 import Pkg
 Pkg.activate(".")
 
-import Enzyme
-dir = joinpath(dirname(dirname(pathof(Enzyme))), "deps")
+
+Enzyme_path = Base.find_package("Enzyme")
+if Enzyme_path === nothing
+    error("First instantiate Project")
+end
+dir = joinpath(dirname(dirname(Enzyme_path)), "deps")
 
 @info "Building Enzyme from master"
 
 run(`$(Base.julia_cmd()) --project=$(dir) -e 'import Pkg; Pkg.instantiate()'`)
 run(`$(Base.julia_cmd()) --project=$(dir) $(dir)/build_local.jl`)
+
+cp(joinpath(dirname(dir), "LocalPreferences.toml"), "LocalPreferences.toml", force=true)
 
 import MPI
 MPI.install_mpiexecjl(;destdir=".", force=true)
